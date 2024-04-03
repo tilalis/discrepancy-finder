6b82e64b43b99621a0dc5a49c77b4a918cc65aee
Aliaksandr Valshtein <alexvolshtein@gmail.com>
2024-04-03 17:36:58 +0300

    [feat]: create Settings object, utilizing pydantic-settings.BaseSettings model

---
+++ src/discrepancy_finder/settings.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseSettings(BaseModel):
+    url: str
+    user: str
+    password: str
+    name: str
+    scheme: str = 'mongodb+srv'
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix='DF_', case_sensitive=False, env_nested_delimiter='__')
+
+    database: DatabaseSettings
+
+    @property
+    def mongo_url(self):
+        return f"{self.database.scheme}://{self.database.user}:{self.database.password}@{self.database.url}"
