47d8f0371c7e5895afc4f8e61ea8597b7f48a5a8
Aliaksandr Valshtein <alexvolshtein@gmail.com>
2024-04-05 10:08:41 +0300

    [feat]: use logic handlers in main function

--- src/discrepancy_finder/__main__.py
+++ src/discrepancy_finder/__main__.py
@@ -6,10 +6,9 @@ from loguru import logger
 from pymongo import MongoClient
 
 from .handlers import (
+    DatabaseInsertHandler,
     DirectoryParsingHandler,
     DiscrepancyFinderHandler,
-    DiscrepancyInsertDatabaseHandler,
-    DocumentsDatabaseInsertHandler,
 )
 from .models.discrepancy import (
     DateIsTooFarInTheFutureOrMissing,
@@ -22,6 +21,7 @@ from .validator import DiscrepancyFinder
 
 logger = logger.patch(lambda record: record.update(name='discrepancy_finder'))
 settings = Settings()
+repository_factory = RepositoryFactory(MongoClient(settings.mongo_url), settings.database.name)
 
 
 def main():
@@ -30,31 +30,27 @@ def main():
     args = parser.parse_args()
 
     logger.info(f"connecting to the database at {settings.database.url}")
-
-    repository_factory = RepositoryFactory(MongoClient(settings.mongo_url), settings.database.name)
     document_repository, discrepancy_repository = (
         repository_factory.create_document_repository(), repository_factory.create_discrepancy_repository()
     )
 
-    handler = DirectoryParsingHandler()
-
-    handler.chain(
-        DocumentsDatabaseInsertHandler(repository=document_repository)
-    ).chain(
-        DiscrepancyFinderHandler(
-            repository=document_repository,
-            discrepancy_finder=DiscrepancyFinder(
-                TitleIsShorterThanPermittedOrMissing(min_length=2),
-                DateIsTooFarInTheFutureOrMissing(max_date=datetime(2023, 1, 1)),
-                FirstRowSumIsHigherThanPermitted(max_sum=5220),
-            )
-        )
-    ).chain(
-        DiscrepancyInsertDatabaseHandler(repository=discrepancy_repository)
+    discrepancy_finder = DiscrepancyFinder(
+        TitleIsShorterThanPermittedOrMissing(min_length=2),
+        DateIsTooFarInTheFutureOrMissing(max_date=datetime(2023, 1, 1)),
+        FirstRowSumIsHigherThanPermitted(max_sum=5220),
     )
 
     try:
-        handler.handle(args.directory)
+        DirectoryParsingHandler().chain(
+            DatabaseInsertHandler(repository=document_repository).chain(
+                DiscrepancyFinderHandler(
+                    repository=document_repository,
+                    discrepancy_finder=discrepancy_finder
+                ).chain(
+                    DatabaseInsertHandler(repository=discrepancy_repository)
+                )
+            )
+        ).handle(args.directory)
     except Exception as e:
         logger.exception(f"an error occurred: {e}")
 
